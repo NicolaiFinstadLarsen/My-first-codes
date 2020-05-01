b8feac11c5abe0ce25e4333bacb8b8b703ee783b
Nicolai Finstad Larsen <59092220+NicolaiFinstadLarsen@users.noreply.github.com>
2020-05-01 12:35:56 +0200

    Added time.sleep

--- guess_the_number.py
+++ guess_the_number.py
@@ -2,6 +2,7 @@
 
 
 import random 
+import time
 
 print(" \nI want you to guess the number between a start and a stop point. \nKeep going! \nTo stop write 00.1")
 
@@ -27,3 +28,5 @@ while ans != x:
 
 if x == ans:
   print("Wow, amazing!")
+  
+time.sleep(10)
